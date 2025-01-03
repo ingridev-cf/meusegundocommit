e6769f67538db85cfbaeb0adeef4e50ba4de9e30
ingridev-cf <ingrid.melo27@gmail.com>
2025-01-03 17:28:51 -0300

    projeto criado

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-let resultado = calcularRank(100, 80)
+let nomeHeroi = "Eduardinho Berserker"
+let resultado = calcularRank(200, 95)
+let nivelHeroi
 
 function calcularRank(numVitorias, numDerrotas){
-    let pontuacao = numVitorias - numDerrotas
-    return pontuacao
-
+    let saldoVitorias = numVitorias - numDerrotas
+    return saldoVitorias
 }
 
-console.log("Sua pontuação é: " + resultado)
+switch (true){
+    case resultado <= 10:
+        nivelHeroi = "Ferro"
+        break
+    case resultado >= 11 && resultado <= 20:
+        nivelHeroi = "Bronze"
+        break
+    case resultado >= 21 && resultado <= 50:
+        nivelHeroi = "Prata"
+        break
+    case resultado >= 51 && resultado <= 80:
+        nivelHeroi = "Ouro"
+        break
+    case resultado >= 81 && resultado <= 90:
+        nivelHeroi = "Diamante"
+        break
+    case resultado >= 91 && resultado <= 100:
+        nivelHeroi = "Lendário"
+        break
+    case resultado >= 101:
+        nivelHeroi = "Imortal"
+        break
+    default:
+        nivelHeroi = "Desconhecido"
+}
 
-
+console.log("O Herói " + nomeHeroi + " tem saldo de " + resultado + " vitórias e está no nível " + nivelHeroi)
     
